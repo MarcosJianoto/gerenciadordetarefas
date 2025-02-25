c55a0a628dcbd0ef8992e39fc66c92cd2c5291f0
Marcos Jianoto <marcos_jianotoo@hotmail.com>
2025-02-24 22:15:42 -0300

    controllers/TasksController.java finalizados.

---
+++ src/main/java/com/gerenciadordetarefas/controllers/TasksController.java
@@ -0,0 +1,70 @@
+package com.gerenciadordetarefas.controllers;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gerenciadordetarefas.dto.TasksDTO;
+import com.gerenciadordetarefas.services.TasksService;
+
+@RestController
+public class TasksController {
+
+	private final TasksService tasksService;
+
+	public TasksController(TasksService tasksService) {
+		this.tasksService = tasksService;
+	}
+
+	@PostMapping("/tasks")
+	public ResponseEntity<Void> saveCategory(@RequestBody TasksDTO tasksDTO) {
+
+		tasksService.saveTask(tasksDTO);
+		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping("/tasks/{id}")
+	public ResponseEntity<Void> updateTask(@RequestBody TasksDTO tasksDTO, @PathVariable Integer id) {
+
+		tasksService.editTask(id, tasksDTO);
+		return ResponseEntity.noContent().build();
+	}
+
+	@GetMapping("/tasks/{id}")
+	public ResponseEntity<TasksDTO> getTasksById(@PathVariable Integer id) {
+		TasksDTO tasksDTO = tasksService.getTaskById(id);
+		return ResponseEntity.ok().body(tasksDTO);
+	}
+
+	@GetMapping("/tasks/all")
+	public ResponseEntity<List<TasksDTO>> getAllTasks() {
+		List<TasksDTO> tasksDTO = tasksService.getAllTasks();
+		return ResponseEntity.ok().body(tasksDTO);
+	}
+
+	@GetMapping("/tasks/diary")
+	public ResponseEntity<List<TasksDTO>> getTasksDiary() {
+		List<TasksDTO> tasksDTO = tasksService.getTasksDiary();
+		return ResponseEntity.ok().body(tasksDTO);
+	}
+
+	@GetMapping("/tasks/diary{date}")
+	public ResponseEntity<List<TasksDTO>> getTasksDiaryByDate(@PathVariable LocalDate date) {
+		List<TasksDTO> tasksDTO = tasksService.getTasksDiaryByDate(date);
+		return ResponseEntity.ok().body(tasksDTO);
+	}
+
+	@DeleteMapping("/tasks/{id}")
+	public ResponseEntity<Void> deleteTask(@PathVariable Integer id) {
+		tasksService.deleteTasks(id);
+		return ResponseEntity.noContent().build();
+	}
+}
